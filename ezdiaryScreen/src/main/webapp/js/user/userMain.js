d6db5089b3b1bf801df5dd39fd1aeca05089932a
wjdqja5288 <wjdqja5288@gmail.com>
2022-01-24 23:13:12 +0900

    fade in fade out 싫다

--- ezdiaryScreen/src/main/webapp/js/user/userMain.js
+++ ezdiaryScreen/src/main/webapp/js/user/userMain.js
@@ -27,22 +27,16 @@ $(document).ready(function() {
 
 
 $(function() { 
-	$(".items_btn").on("click", function() { 
+	$("#start_btn1").on("click", function() { 
 		// id가 "divBox"인 요소를 0.5초에 걸쳐 사라지게 하고 
 		// 1초의 지연시간 뒤에 다시 2초에 걸쳐 나타나게 함. 
-		$("#divBox").fadeOut(500).delay(1000).fadeIn(2000); 
+		$("#divBox").fadeOut(2000).delay(1000).$("#divBox2").fadeIn(2000).finish(); 
+	}); 
+	$("#start_btn2").on("click", function() { 
+		// id가 "divBox"인 요소를 0.5초에 걸쳐 사라지게 하고 
+		// 1초의 지연시간 뒤에 다시 2초에 걸쳐 나타나게 함. 
+		$("#divBox2").fadeOut(2000).delay(1000).$("#divBox3").fadeIn(2000); 
 	}); 
 });
 
 
-
-$(function() {
-  $("#fadeInBtn").on("click", function() {
-    $("#divBox").fadeIn("slow");      
-  });
-   
-
-  $("#fadeOutBtn").on("click", function() {
-    $("#divBox").fadeOut("fast");      
-  });
-});
