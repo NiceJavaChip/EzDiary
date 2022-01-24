807011ebb6a793dc8082a437fa24c27eae648766
wjdqja5288 <wjdqja5288@gmail.com>
2022-01-24 22:38:27 +0900

    화가난다~

--- ezdiaryScreen/src/main/webapp/js/user/userMain.js
+++ ezdiaryScreen/src/main/webapp/js/user/userMain.js
@@ -1,8 +1,18 @@
+// 엔터키 이벤트(닉네임 페이지)
 function enter(){
 	if(event.keyCode==13){
 		location.href="userSurvey.jsp";
 	}
 }
+
+function click(){
+	var link = 'userResultloading.jsp';
+		location.href=link;
+}
+
+///////////////////////////////////////////////////////////
+
+
 // 글자 수 세기
 $(document).ready(function() {
     $('#test').on('keyup', function() {
@@ -13,4 +23,26 @@ $(document).ready(function() {
             $('#test_cnt').html("(1000 / 1000)");
         }
     });
+});
+
+
+$(function() { 
+	$(".items_btn").on("click", function() { 
+		// id가 "divBox"인 요소를 0.5초에 걸쳐 사라지게 하고 
+		// 1초의 지연시간 뒤에 다시 2초에 걸쳐 나타나게 함. 
+		$("#divBox").fadeOut(500).delay(1000).fadeIn(2000); 
+	}); 
+});
+
+
+
+$(function() {
+  $("#fadeInBtn").on("click", function() {
+    $("#divBox").fadeIn("slow");      
+  });
+   
+
+  $("#fadeOutBtn").on("click", function() {
+    $("#divBox").fadeOut("fast");      
+  });
 });
